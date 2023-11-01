714778c2c9aa162893ffd8e6248c20e3e37ce448
22DP3TBark <131261277+22DP3TBark@users.noreply.github.com>
2023-11-01 10:12:13 +0000

    7 uzdevums

---
+++ 7uzdevums.py
@@ -0,0 +1,9 @@
+x = int(input("X: "))
+y = int(input("y: "))
+
+if x > -1 and x < 3 and y > -2 and y < 1:
+    print("ieksa")
+elif x == -1 or x == 3 and y>= -2 and y <= 1:
+    print("uz robež līnijas ")
+else:
+    print("Ārpus")  
