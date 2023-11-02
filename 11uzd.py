011b1373427c94623dd18aebc47716fe1e800484
22DP3TBark <131261277+22DP3TBark@users.noreply.github.com>
2023-11-02 17:08:00 +0000

    11 uzd nav pabeigts

---
+++ 11uzd.py
@@ -0,0 +1,47 @@
+"""
+Autors:  Indrīķis Teodors Barkāns
+DP2-3
+Programma pārbauda punkta koordinates figurai. Trapeces koordinātes ir -3;2, -2,5;8 , 4;8, 8,2;2 
+"""
+ 
+import doctest
+
+def check_coordinates(punkts_x, punkts_y: float) -> str:
+    """
+    Funkcija pārbauda punkta koordinates (punkta_x; punkta_y) figūrai Trapecei ar koordinātēm -3;2, -2.5;8, 4;8, 8.2;2
+
+    parameters 
+    -----------
+    punkta_x : float
+        punkta x koordinate Dekartu plakne.
+    punkta_y : float
+        punkta x koordinate Dekartu plakne. 
+
+    returns
+    --------------
+    str 
+        iekšā, uz līnijas, ārpuss
+
+    >>> check_coordinates(1,1)
+    'ārpuss'
+    >>> check_coordinates(4,4)
+    'iekšā'
+    >>> check_coordinates(-3,2)
+    'uz līnijas'
+    >>> check_coordinates(1,8)
+    'uz līnijas'
+    >>> check_coordinates(6,5.1)
+    'uz līnijas'
+    >>> check_coordinates(-4,3)
+    'ārpuss'
+    """
+    if punkts_x > -3 and punkts_x < 8.2 and punkts_y > 2 and punkts_y < 8 and punkts_y > 12 * punkts_x + 38 and punkts_y < 1.428571 * punkts_x + 13.714:
+        return "iekšā"
+    elif punkts_x == -3 or punkts_x == 8.2 or punkts_y == 2 or punkts_y == 8 or punkts_y == 12 * punkts_x + 38 or punkts_y == 1.428571 * punkts_x + 13.714:
+        return "uz līnijas"
+    else:
+        return "ārpuss"
+
+   
+doctest.testmod(verbose=True)
+    
